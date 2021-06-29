2f5f30d56745be8eaa8c61c7a993fdb148806b6f
Martin Linklater <mslinklater@gmail.com>
2021-06-29 18:08:06 +0100

    Initial EightBitHero

---
+++ EightBitHero/Assets/Scripts/Pieces/MOS6502.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOS6502 : MonoBehaviour
+{
+#region Registers
+    public byte Acc;
+    public byte X;
+    public byte Y;
+	public byte flags;
+	public ushort PC;
+#endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Tick()
+    {
+
+    }
+}
