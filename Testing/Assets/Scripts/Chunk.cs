2d77e3c62d27b2e40f32e4b95c67e4b370eef883
Martin Linklater <mslinklater@gmail.com>
2024-02-29 23:25:56 +0000

    Discreet

--- Testing/Assets/Scripts/Chunk.cs
+++ Testing/Assets/Scripts/Chunk.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
+using UnityEditor.Overlays;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -10,43 +11,26 @@ using UnityEngine.Assertions;
 
 public class Chunk : MonoBehaviour
 {
-    public enum ChunkMode {
-        Continuous,
-        Discreet
-    }
-
-    public enum CubeCorner {
-        MinX_MinY_MinZ,
-        MaxX_MinY_MinZ,
-        MinX_MaxY_MinZ,
-        MaxX_MaxY_MinZ,
-        MinX_MinY_MaxZ,
-        MaxX_MinY_MaxZ,
-        MinX_MaxY_MaxZ,
-        MaxX_MaxY_MaxZ
-    }
-
-    public ChunkMode chunkMode = ChunkMode.Continuous;
     public bool showCage = false;
+    public bool removeInternalFaces = false;
     public float cubeSize = 1.0f;
 
-    [Range(1,39)]   // 39 max as this means 40*40*40 verts, which is 64K
-    public int cubeCount = 5;
+    [Range(1,20)]   // 20 max as this means 20*20*20*8 verts, which is 64K
+    public int cubeCount = 2;
     private int numCubes = 0;
-    private int vertAxisCount = 6;
 
     private MeshFilter meshFilter;
+
+    private Cube[] cubes;
 
     private Vector3[] meshVertices;
     private Color32[] meshColors;
     private List<int> meshIndices = new List<int>();
-
     private bool[] cubeOccupied;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Chunk.Start()");
         // rebuild
         RebuildMesh();
     }
@@ -57,120 +41,114 @@ public class Chunk : MonoBehaviour
         
     }
 
-    private void BuildDiscreetMesh()
+    private void BuildCubeArray()
     {
-
-    }
-
-    private void BuildContinuousMesh()
-    {
-        int numVerts = vertAxisCount * vertAxisCount * vertAxisCount;
-        cubeOccupied = new bool[numCubes];
-   
-        meshVertices = new Vector3[numVerts];
-        meshColors = new Color32[numVerts];
-
-        meshFilter.sharedMesh.triangles = new int[0];
-
-        // build vertices array
-        int i = 0;
-        for(int z = 0; z < vertAxisCount ; z++)
-        {
-            for(int y = 0; y < vertAxisCount; y++)
-            {
-                for(int x = 0; x < vertAxisCount; x++)
-                {
-//                    cubeOccupied[i] = UnityEngine.Random.Range(0.0f, 1.0f) > 0.5f;
-                    meshVertices[i] = new Vector3(x * cubeSize, y * cubeSize, z * cubeSize);
-                    meshColors[i] = new Color32((byte)(x * 255 / cubeCount), (byte)(y * 255 / cubeCount), (byte)(z * 255 / cubeCount), 255);
-                    i++;
-                }
-            }
-        }
-        meshFilter.sharedMesh.vertices = meshVertices;
-        meshFilter.sharedMesh.colors32 = meshColors;
-
-        // occupancy
-        for(int z = 0; z < cubeCount ; z++)
+        int iCube = 0;
+        for(int z = 0; z < cubeCount; z++)
         {
             for(int y = 0; y < cubeCount; y++)
             {
                 for(int x = 0; x < cubeCount; x++)
                 {
-                    cubeOccupied[x + (y * cubeCount) + (z * cubeCount * cubeCount)] = UnityEngine.Random.Range(0.0f, 1.0f) > 0.5f;
+                    int xp = x + 1;
+                    int yp = y + 1;
+                    int zp = z + 1;
+
+                    Cube cube = new Cube();
+                    cube.firstVertIndex = iCube * 8;
+//                    cube.occupied = UnityEngine.Random.Range(0.0f, 1.0f) > 0.5f;
+                    cube.occupied = true;
+//                    cube.numVerts = 8;
+                    cube.verts = new Vector3[8];
+                    cube.verts[0] = new Vector3(x * cubeSize, y * cubeSize, z * cubeSize);
+                    cube.verts[1] = new Vector3(xp * cubeSize, y * cubeSize, z * cubeSize);
+                    cube.verts[2] = new Vector3(x * cubeSize, yp * cubeSize, z * cubeSize);
+                    cube.verts[3] = new Vector3(xp * cubeSize, yp * cubeSize, z * cubeSize);
+                    cube.verts[4] = new Vector3(x * cubeSize, y * cubeSize, zp * cubeSize);
+                    cube.verts[5] = new Vector3(xp * cubeSize, y * cubeSize, zp * cubeSize);
+                    cube.verts[6] = new Vector3(x * cubeSize, yp * cubeSize, zp * cubeSize);
+                    cube.verts[7] = new Vector3(xp * cubeSize, yp * cubeSize, zp * cubeSize);
+
+                    cube.color = new Color32((byte)UnityEngine.Random.Range(0.0f, 255.0f), 
+                                                (byte)UnityEngine.Random.Range(0.0f, 255.0f),
+                                                (byte)UnityEngine.Random.Range(0.0f, 255.0f), 255);
+
+                    cubes[iCube++] = cube;
                 }
             }
         }
+    }
 
-        // create triangles
-        meshIndices.Clear();
-
-        for(int z = 0 ; z < cubeCount ; z++)
+    private void BuildAllFaces()
+    {
+        int iCube = 0;
+        for(int z = 0; z < cubeCount; z++)
         {
-            for(int y = 0 ; y < cubeCount ; y++)
+            for(int y = 0; y < cubeCount; y++)
             {
-                for(int x = 0 ; x < cubeCount ; x++)
+                for(int x = 0; x < cubeCount; x++)
                 {
-                    if(cubeOccupied[x + (y * cubeCount) + (z * cubeCount * cubeCount)])
+                    if(cubes[iCube].occupied)
                     {
-                        // Z min face
-                        meshIndices.Add(IndexForPos(x,y,z, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x, y+1, z, CubeCorner.MinX_MinY_MinZ));                    
-                        meshIndices.Add(IndexForPos(x+1, y, z, CubeCorner.MinX_MinY_MinZ));
+                        int firstIndex = cubes[iCube].firstVertIndex;
 
-                        meshIndices.Add(IndexForPos(x, y+1, z, CubeCorner.MinX_MinY_MinZ));                    
-                        meshIndices.Add(IndexForPos(x+1, y+1, z, CubeCorner.MinX_MinY_MinZ));                    
-                        meshIndices.Add(IndexForPos(x+1, y, z, CubeCorner.MinX_MinY_MinZ));
+                        // Z min
+                        meshIndices.Add(firstIndex);
+                        meshIndices.Add(firstIndex + 2);
+                        meshIndices.Add(firstIndex + 1);
+                        meshIndices.Add(firstIndex + 2);
+                        meshIndices.Add(firstIndex + 3);
+                        meshIndices.Add(firstIndex + 1);
 
-                        // Z max face
-                        meshIndices.Add(IndexForPos(x,y,z+1, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x+1, y, z+1, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x, y+1, z+1, CubeCorner.MinX_MinY_MinZ));                    
+                        // Z max
+                        meshIndices.Add(firstIndex + 4);
+                        meshIndices.Add(firstIndex + 5);
+                        meshIndices.Add(firstIndex + 6);
+                        meshIndices.Add(firstIndex + 6);
+                        meshIndices.Add(firstIndex + 5);
+                        meshIndices.Add(firstIndex + 7);
 
-                        meshIndices.Add(IndexForPos(x, y+1, z+1, CubeCorner.MinX_MinY_MinZ));                    
-                        meshIndices.Add(IndexForPos(x+1, y, z+1, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x+1, y+1, z+1, CubeCorner.MinX_MinY_MinZ));                    
+                        // Y min
+                        meshIndices.Add(firstIndex);
+                        meshIndices.Add(firstIndex + 1);
+                        meshIndices.Add(firstIndex + 4);
+                        meshIndices.Add(firstIndex + 1);
+                        meshIndices.Add(firstIndex + 5);
+                        meshIndices.Add(firstIndex + 4);
 
-                        // X min face
-                        meshIndices.Add(IndexForPos(x,y,z, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x, y, z+1, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x, y+1, z, CubeCorner.MinX_MinY_MinZ));                    
+                        // Y max
+                        meshIndices.Add(firstIndex + 2);
+                        meshIndices.Add(firstIndex + 6);
+                        meshIndices.Add(firstIndex + 3);
+                        meshIndices.Add(firstIndex + 3);
+                        meshIndices.Add(firstIndex + 6);
+                        meshIndices.Add(firstIndex + 7);
 
-                        meshIndices.Add(IndexForPos(x, y, z+1, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x, y+1, z+1, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x, y+1, z, CubeCorner.MinX_MinY_MinZ));                    
+                        // X min
+                        meshIndices.Add(firstIndex);
+                        meshIndices.Add(firstIndex + 4);
+                        meshIndices.Add(firstIndex + 2);
+                        meshIndices.Add(firstIndex + 4);
+                        meshIndices.Add(firstIndex + 6);
+                        meshIndices.Add(firstIndex + 2);
 
-                        // // X max face
-                        meshIndices.Add(IndexForPos(x+1,y,z, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x+1, y+1, z, CubeCorner.MinX_MinY_MinZ));                    
-                        meshIndices.Add(IndexForPos(x+1, y, z+1, CubeCorner.MinX_MinY_MinZ));
-
-                        meshIndices.Add(IndexForPos(x+1, y, z+1, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x+1, y+1, z, CubeCorner.MinX_MinY_MinZ));                    
-                        meshIndices.Add(IndexForPos(x+1, y+1, z+1, CubeCorner.MinX_MinY_MinZ));
-
-                        // Y min face
-                        meshIndices.Add(IndexForPos(x,y,z, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x+1, y, z, CubeCorner.MinX_MinY_MinZ));                    
-                        meshIndices.Add(IndexForPos(x, y, z+1, CubeCorner.MinX_MinY_MinZ));
-
-                        meshIndices.Add(IndexForPos(x+1, y, z, CubeCorner.MinX_MinY_MinZ));                    
-                        meshIndices.Add(IndexForPos(x+1, y, z+1, CubeCorner.MinX_MinY_MinZ));                    
-                        meshIndices.Add(IndexForPos(x, y, z+1, CubeCorner.MinX_MinY_MinZ));
-
-                        // Y max face
-                        meshIndices.Add(IndexForPos(x,y+1,z, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x, y+1, z+1, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x+1, y+1, z, CubeCorner.MinX_MinY_MinZ));                    
-
-                        meshIndices.Add(IndexForPos(x+1, y+1, z, CubeCorner.MinX_MinY_MinZ));                    
-                        meshIndices.Add(IndexForPos(x, y+1, z+1, CubeCorner.MinX_MinY_MinZ));
-                        meshIndices.Add(IndexForPos(x+1, y+1, z+1, CubeCorner.MinX_MinY_MinZ));                    
+                        // X max
+                        meshIndices.Add(firstIndex + 1);
+                        meshIndices.Add(firstIndex + 3);
+                        meshIndices.Add(firstIndex + 5);
+                        meshIndices.Add(firstIndex + 5);
+                        meshIndices.Add(firstIndex + 3);
+                        meshIndices.Add(firstIndex + 7);
                     }
-                }                
+                    iCube++;
+                }
             }
         }
-        meshFilter.sharedMesh.triangles = meshIndices.ToArray();
+    }
+
+    private void BuildVisibleFaces()
+    {
+
     }
 
     public void RebuildMesh()
@@ -181,95 +159,51 @@ public class Chunk : MonoBehaviour
         {
             meshFilter = gameObject.AddComponent<MeshFilter>();
         }
+        meshFilter.sharedMesh.triangles = new int[0];
 
         numCubes = cubeCount * cubeCount * cubeCount;
-        vertAxisCount = cubeCount + 1;
+        cubes = new Cube[numCubes];
 
-        if( chunkMode == ChunkMode.Continuous)
-        {
-            BuildContinuousMesh();
-        }
-        else
-        {
-            BuildDiscreetMesh();
-        }
-    }
+        BuildCubeArray();
 
-    private int IndexForPos(int x, int y, int z, CubeCorner corner)
-    {
-        if(chunkMode == ChunkMode.Continuous)
+        // build mesh vertex array
+        int numVertices = numCubes * 8;
+        meshVertices = new Vector3[numVertices];
+        meshColors = new Color32[numVertices];
+
+        int vertNum = 0;
+        for(int i = 0; i < numCubes ; i++)
         {
-            switch(corner)
+            for(int iVert = 0; iVert < 8; iVert++)
             {
-                case CubeCorner.MinX_MinY_MinZ:
-                    break;
-                case CubeCorner.MaxX_MinY_MinZ:
-                    x += 1;
-                    break;
-                case CubeCorner.MinX_MaxY_MinZ:
-                    y += 1;
-                    break;
-                case CubeCorner.MaxX_MaxY_MinZ:
-                    x += 1;
-                    y += 1;
-                    break;
-                case CubeCorner.MinX_MinY_MaxZ:
-                    z += 1;
-                    break;
-                case CubeCorner.MaxX_MinY_MaxZ:
-                    x += 1;
-                    z += 1;
-                    break;
-                case CubeCorner.MinX_MaxY_MaxZ:
-                    y += 1;
-                    z += 1;
-                    break;
-                case CubeCorner.MaxX_MaxY_MaxZ:
-                    x += 1;
-                    y += 1;
-                    z += 1;
-                    break;
+                meshVertices[vertNum] = cubes[i].verts[iVert];
+                meshColors[vertNum] = cubes[i].color;
+                vertNum++;
             }
-            int ret = 0;
-            ret += x;
-            ret += y * vertAxisCount;
-            ret += z * vertAxisCount * vertAxisCount;
-            return ret;
+        }
+        meshFilter.sharedMesh.vertices = meshVertices;
+        meshFilter.sharedMesh.colors32 = meshColors;
+
+        // build mesh indices
+        meshIndices.Clear();
+
+        if(removeInternalFaces)
+        {
+            BuildVisibleFaces();
         }
         else
         {
-            Debug.LogError("TODO");
-            return 0;
+            BuildAllFaces();
         }
+
+        Debug.Log("Triangles size: " + meshIndices.Count());
+        meshFilter.sharedMesh.triangles = meshIndices.ToArray();
     }
 
     public void OnDrawGizmosSelected()
     {
         if( showCage )
         {
-            for( int i = 0 ; i < vertAxisCount ; i++ )
-            {   
-                // min Z
-                Debug.DrawLine(meshVertices[IndexForPos(i, 0, 0, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(i, cubeCount, 0, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-                Debug.DrawLine(meshVertices[IndexForPos(0, i, 0, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(cubeCount, i, 0, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-                // max Z
-                Debug.DrawLine(meshVertices[IndexForPos(i, 0, cubeCount, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(i, cubeCount, cubeCount, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-                Debug.DrawLine(meshVertices[IndexForPos(0, i, cubeCount, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(cubeCount, i, cubeCount, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-
-                // min X
-                Debug.DrawLine(meshVertices[IndexForPos(0, i, 0, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(0, i, cubeCount, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-                Debug.DrawLine(meshVertices[IndexForPos(0, 0, i, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(0, cubeCount, i, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-                // max X
-                Debug.DrawLine(meshVertices[IndexForPos(cubeCount, i, 0, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(cubeCount, i, cubeCount, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-                Debug.DrawLine(meshVertices[IndexForPos(cubeCount, 0, i, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(cubeCount, cubeCount, i, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-
-                // min Y
-                Debug.DrawLine(meshVertices[IndexForPos(i, 0, 0, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(i, 0, cubeCount, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-                Debug.DrawLine(meshVertices[IndexForPos(0, 0, i, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(cubeCount, 0, i, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-                // max Y
-                Debug.DrawLine(meshVertices[IndexForPos(i, cubeCount, 0, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(i, cubeCount, cubeCount, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-                Debug.DrawLine(meshVertices[IndexForPos(0, cubeCount, i, CubeCorner.MinX_MinY_MinZ)] + transform.position, meshVertices[IndexForPos(cubeCount, cubeCount, i, CubeCorner.MinX_MinY_MinZ)] + transform.position);
-            }
         }
     }
 }
