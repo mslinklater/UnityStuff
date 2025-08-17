6edbec1b2989f980345303987f7fd7798dd15430
Martin Linklater <mslinklater@gmail.com>
2025-08-17 21:59:16 +0100

    Started again on 6.2

--- JettellyShadersAndProceduralShapes/Assets/Jettelly Books/Shared/Matcap/Matcap.hlsl
+++
@@ -1,8 +0,0 @@
-void Matcap_float(in Texture2D<float4> MainTex, in SamplerState SS, in float3 NormalVS, in float Opacity, out float3 RGB)
-{
-    const half c = 0.5;    
-    float2 uv = ((NormalVS * c) + c).xy;
-    float3 texture_RGB = SAMPLE_TEXTURE2D(MainTex, SS, uv).rgb;
-    texture_RGB.rgb *= Opacity;
-    RGB = texture_RGB;
-}
--- JettellyShadersAndProceduralShapes/Assets/Jettelly Books/Shared/PolarCoordinates/PolarCoordinates.hlsl
+++
@@ -1,7 +0,0 @@
-void Unity_PolarCoordinates_float(float2 UV, float2 Center, float RadialScale, float LengthScale, out float2 Out)
-{
-    float2 delta = UV - Center;
-    float radius = length(delta) * 2 * RadialScale;
-    float angle = atan2(delta.x, delta.y) * 1.0/6.28 * LengthScale;
-    Out = float2(radius, angle);
-}
